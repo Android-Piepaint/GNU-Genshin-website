d548aed37c5b9b6be0e22d8eed528eb21409d50f
Android-Piepaint <73344795+Android-Piepaint@users.noreply.github.com>
2022-12-30 15:10:02 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,16 +13,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-drupal`,
-      options: {
-        baseUrl: process.env.DRUPAL_BASE_URL,
-        basicAuth: {
-          username: process.env.DRUPAL_BASIC_AUTH_USERNAME,
-          password: process.env.DRUPAL_BASIC_AUTH_PASSWORD,
-        },
-        fastBuilds: true,
-      },
-    },
+
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
     "gatsby-transformer-sharp",
